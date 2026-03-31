11fdb838df6b8102ed35a8bc893d1fd03e5c1697
murilocost4 <murilocoimbr4@icloud.com>
2026-03-31 09:10:16 -0300

    analytics implementado corretamente

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -28,6 +28,7 @@ export default function RootLayout({ children }) {
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
+      <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID} />
     </html>
   );
 }
